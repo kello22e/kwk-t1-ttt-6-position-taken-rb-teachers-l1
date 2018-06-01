914d5383ae55e22a467f29d6e53157a6f0d54261
kello22e <kello22e@mtholyoke.edu>
2018-06-01 17:51:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(arr,index)
+  if arr[index] == "X" || arr[index] == "O"
+    return TRUE
+  elsif arr[index] == " " || arr[index] == ""
+    return FALSE
+  else arr[index] == NIL
+    return FALSE
+  end
+end
